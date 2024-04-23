d849b30351a1cbb6d5197635f5e65eb450e017a3
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-23 14:31:10 +0200

    fixed image import

--- src/Components/FourOFour/fourOFourContent.jsx
+++ src/Components/FourOFour/fourOFourContent.jsx
@@ -1,8 +1,10 @@
 import styles from "./fourOFour.module.css";
+import pikachu from "../../assets/images/pikachu.gif";
+
 function FourOFourContent() {
   return (
     <div className={styles.errorDiv}>
-      <img src="/src/assets/images/pikachu.gif" alt="pikachu-gif" />
+      <img src={pikachu} alt="pikachu-gif" />
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>

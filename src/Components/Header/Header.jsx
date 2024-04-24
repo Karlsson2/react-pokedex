6d48873e3d250206cac13095ab631b759c168301
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-24 09:14:03 +0200

    fixed searchbar and styled header, added some image assets.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import RandomPokemon from "../RandomPokemon/RandomPokemon";
 import styles from "./Header.module.css";
+import Search from "../Search/Search";
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <a href="/">
-        <h1>Pokemon</h1>
+        <h1>Pokedex</h1>
       </a>
       <RandomPokemon />
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import List from "../Components/List/List";
 import ToTop from "../Components/ToTop/ToTop";
+import Search from "../Components/Search/Search";
 
 function Home() {
   return (
     <>
+      <Search />
+
       <ToTop />
       <List />
     </>
